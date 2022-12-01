6bc7ea4896d62be161917166a3c41f8a9c5f4e04
Bryan Oppong-Boateng <bryan@bryanboateng.com>
2022-12-02 00:12:50 +0100

    Convert Package.swift to Tabs

--- Package.swift
+++ Package.swift
@@ -4,28 +4,28 @@
 import PackageDescription
 
 let package = Package(
-    name: "AdventOfCode2022",
-		platforms: [
-			.macOS(.v13)
-		],
-    products: [
-        .executable(
-            name: "AdventOfCode2022",
-            targets: ["AdventOfCode2022"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.0"),
-        .package(url: "https://github.com/apple/swift-algorithms", from: "1.0.0"), 
-    ],
-    targets: [
-        .executableTarget(
-            name: "AdventOfCode2022",
-            dependencies: [
-                .product(name: "ArgumentParser", package: "swift-argument-parser"),
-                .product(name: "Algorithms", package: "swift-algorithms"),
-            ],
-            resources: [
-                .process("Inputs")
-            ]),
-    ]
+	name: "AdventOfCode2022",
+	platforms: [
+		.macOS(.v13)
+	],
+	products: [
+		.executable(
+			name: "AdventOfCode2022",
+			targets: ["AdventOfCode2022"]),
+	],
+	dependencies: [
+		.package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.0"),
+		.package(url: "https://github.com/apple/swift-algorithms", from: "1.0.0"), 
+	],
+	targets: [
+		.executableTarget(
+			name: "AdventOfCode2022",
+			dependencies: [
+				.product(name: "ArgumentParser", package: "swift-argument-parser"),
+				.product(name: "Algorithms", package: "swift-algorithms"),
+			],
+			resources: [
+				.process("Inputs")
+			]),
+	]
 )
