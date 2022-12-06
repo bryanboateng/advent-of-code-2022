cd7b9f2c5fd189ea2a1d4cf9e0108b54d118afdf
Bryan Oppong-Boateng <bryan@bryanboateng.com>
2022-12-06 17:48:46 +0100

    Add day 6

--- Sources/AdventOfCode2022/Day06.swift
+++ Sources/AdventOfCode2022/Day06.swift
@@ -1,9 +1,25 @@
 let day06: (Algorithm, Algorithm) = (part1, part2)
 
 private let part1: Algorithm = { input in
-	return "NOT DONE"
+	let markerSize = 4
+	return input
+		.windows(ofCount: markerSize)
+		.enumerated()
+		.first { index, signal in
+			Set(signal).count == markerSize
+		}!
+		.0
+	+ markerSize
 }
 
 private let part2: Algorithm = { input in
-	return "NOT DONE"
+	let markerSize = 14
+	return input
+		.windows(ofCount: markerSize)
+		.enumerated()
+		.first { index, signal in
+			Set(signal).count == markerSize
+		}!
+		.0
+	+ markerSize
 }
